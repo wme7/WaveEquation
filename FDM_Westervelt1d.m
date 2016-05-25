d89277fbf33475ed5a85d3317d34b3a82014ee93
Manuel Diaz <manuel.ade@gmail.com>
2016-05-25 15:19:40 +0800

    minor modifications

--- FDM_Westervelt1d.m
+++ FDM_Westervelt1d.m
@@ -1,7 +1,7 @@
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %         A FDM solver for the Second-order Westervelt Equation
-%                  by Manuel Diaz, NHRI, 2016.05.22
+%                 by Manuel A. Diaz, NHRI, 2016.05.22
 %
 %    p_xx-1/(c^2)*p_tt+delta/(c0^4)*p_ttt+beta/(r0*c0^4)*p^2_tt = 0,  (1)
 %
@@ -14,7 +14,7 @@
 clear; %clc; close all;
 
 %% Parameters
-     cfl = 0.50;	% CFL stability condition
+     cfl = 0.80;	% CFL stability condition
   tFinal = 32E-6;	% final time
 plotfigs = true;	% plot figures?
 
@@ -23,7 +23,7 @@ dx=0.1E-3; % m
 c0=1600; % m/s
 r0=1100; % kg/m^3 
 alpha=4.5; % Np/m, absortion
-beta=5.5; 
+beta=5.5; % 1+B/(2A), where B/A nonlinarity parameter of fluid
 f=1E6; % 1 MHz 
 omega=2*pi*f; % angular freq
 delta=2*c0^2*alpha/omega^2; % sound viscosity
@@ -41,6 +41,7 @@ ax=-2.56E-2; bx=2.56E-2; x=ax+dx/2:dx:bx; nx=length(x);
 dt0=cfl*dx/c0;
 
 % Solution Arrays
+pmmmmm=zeros(1,nx); % previous state of wave
 pmmmm=zeros(1,nx); % previous state of wave
 pmmm=zeros(1,nx); % previous state of wave
 pmm=zeros(1,nx); % previous state of wave
@@ -52,28 +53,43 @@ pn=zeros(1,nx); % next state of wave
 region = [ax,bx,-2.2,2.2];
 
 %% Solver Loop
-t=3*dt0; dt=dt0; it=0; 
+t=6*dt0; dt=dt0; it=0; 
 
+% coefs
+a=(c0*dt0/dx)^2/12; b=(delta/(8*dt0*c0^2)); c=beta/(r0*c0^2);
+
+method = 2;
 while t < tFinal
     
-    % Compute and update [u_old,u_new]
-    for i=3:nx-2
-        pn(i)= 2*pn(i)-pm(i) + ...
-            (c0*dt/dx)^2 *(-po(i+2)+16*po(i+1)-30*po(i)+16*po(i-1)-po(i-2))/12;
+    switch method
+        case 1
+            for i=3:nx-2
+                pn(i)= 2*pn(i)-pm(i) + ...
+                    a*(-po(i+2)+16*po(i+1)-30*po(i)+16*po(i-1)-po(i-2)) + ...
+                    b*(6*po(i)-23*pm(i)+34*pmm(i)-24*pmmm(i)+8*pmmmm(i)-pmmmmm(i)) + ...
+                    c*(2*po(i)^2-5*pm(i)^2+4*pmm(i)^2-pmmm(i)^2);                
+            end
+        case 2
+            for i=3:nx-2
+                pn(i)= 2*pn(i)-pm(i) + ...
+                    a*(-po(i+2)+16*po(i+1)-30*po(i)+16*po(i-1)-po(i-2)) + ...
+                    b*(6*po(i)-23*pm(i)+34*pmm(i)-24*pmmm(i)+8*pmmmm(i)-pmmmmm(i)) + ...
+                    beta*dt^2/(12*r0*dx^2)*(-po(i+2)^2+16*po(i+1)^2-30*po(i)^2+16*po(i-1)^2-po(i-2)^2);
+            end
     end
     
     % update info
-    pm=po; po=pn;
+    pmmmmm=pmmmm; pmmmm=pmmm; pmmm=pmm; pmm=pm; pm=po; po=pn;
     
     % Update dt and iteration counter
-    if t+dt>tFinal; dt=tFinal-t; end; t=t+dt; it=it+1;
+    if t+dt>tFinal; dt=tFinal-t; a=(c0*dt/dx)^2/12; b=(delta/(8*dt*c0^2)); end; t=t+dt; it=it+1; 
     
     % Perturbation in time
-    po(nx/2) = sin(omega*t)*exp(-(t-2E-6)^2);
+    po(nx/2) = sin(omega*t)*exp(-((t-4E-6)/2E-6)^2); 
     
     % plot figure
     if plotfigs~=false
-    plot(x,po,'r.-'); axis(region);
+    plot(x,po,'r.-'); axis(region); grid minor; 
     title(sprintf('time t=%0.2f',t)); drawnow
     end
 end
--- FDM_Westervelt2d.m
+++ FDM_Westervelt2d.m
@@ -1,7 +1,7 @@
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %         A FDM solver for the Second-order Westervelt Equation
-%                  by Manuel Diaz, NHRI, 2016.05.20
+%                 by Manuel A. Diaz, NHRI, 2016.05.20
 %
 % (q_xx+q_rr)-1/(c^2)*p_tt+delta/(c0^4)*p_ttt+beta/(r0*c0^4)*p^2_tt=0, (1)
 %
@@ -12,13 +12,13 @@
 %     ultrasound." The J.Acoust.Soc.Am. 105.5 (1999): L7-L12.  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Note: 
-% The numerical solution of Eq.(1) was calculated on a polar cylindrical
-% grid. The source was modeled as a spherical cap (bowl) having azimuthal
-% symmetry about the axis of the source. The acoustic and temperature
-% fields need only be computed using a two dimensional spatial grid, x, in
-% the axial direction and r in the radial direction 
+%  The numerical solution of Eq.(1) was calculated on a polar cylindrical
+%  grid. The source was modeled as a spherical cap (bowl) having azimuthal
+%  symmetry about the axis of the source. The acoustic and temperature
+%  fields need only be computed using a two dimensional spatial grid, x, in
+%  the axial direction and r in the radial direction 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clc; clear; close all;
+clear; %clc; close all;
 
 %% Parameters
      cfl = 0.50;	% CFL stability condition
@@ -31,7 +31,7 @@ dr=0.1E-3; % m
 c0=1600; % m/s
 r0=1100; % kg/m^3 
 alpha=4.5; % Np/m, absortion
-beta=5.5; 
+beta=5.5; % 1+B/(2A), where B/A nonlinarity parameter of fluid
 f=1E6; % 1 MHz 
 omega=2*pi*f; % angular freq
 delta=2*c0^2*alpha/omega^2; % sound viscosity
@@ -45,18 +45,23 @@ wb=0.5; % kg/(m^3 s)
 % Build mesh
 ax=-2.56E-2; bx=2.56E-2; x=ax+dx/2:dx:bx; nx=length(x);
 ar=-1.00E-2; br=4.12E-2; r=ar+dr/2:dr:br; nr=length(r);
-[X,R]=meshgrid(x,r);
 
 % Build device
+[X,R]=meshgrid(x,r);
 faceA = (X.^2+R.^2-3.00E-2^2) > 0;
 faceB = (X.^2+R.^2-3.01E-2^2) < 0;
 faceC = abs(X)<0.02;
 device= find(faceA.*faceB.*faceC);
+clear X R faceA faceB faceC;
 
 % Initial time step
 dt0=cfl*min(dx/c0,dr/c0);
 
 % Solution Arrays
+pmmmmm=zeros(nr,nx); % previous state of wave
+pmmmm=zeros(nr,nx); % previous state of wave
+pmmm=zeros(nr,nx); % previous state of wave
+pmm=zeros(nr,nx); % previous state of wave
 pm=zeros(nr,nx); % previous state of wave
 po=zeros(nr,nx); % present state of wave
 pn=zeros(nr,nx); % next state of wave
@@ -65,26 +70,33 @@ pn=zeros(nr,nx); % next state of wave
 region = [ax,bx,ar,br,-1.2,1.2];
 
 %% Solver Loop
-t=3*dt0; dt=dt0; it=0;
+t=3*dt0; dt=dt0; it=0; 
+
+% coefs
+a=(c0*dt0/dx)^2/12; b=(c0*dt0/dr)^2/12; c=(delta/(8*dt0*c0^2)); d=beta/(r0*c0^2);
 
 while t < tFinal
         
     for j=3:nr-2
         for i=3:nx-2
             pn(i,j)= 2*pn(i,j)-pm(i,j) + ...
-                (c0*dt/dr)^2 *(-po(i,j+2)+16*po(i,j+1)-30*po(i,j)+16*po(i,j-1)-po(i,j-2))/12 + ...
-                (c0*dt/dx)^2 *(-po(i+2,j)+16*po(i+1,j)-30*po(i,j)+16*po(i-1,j)-po(i-2,j))/12;
+                a*(-po(i,j+2)+16*po(i,j+1)-30*po(i,j)+16*po(i,j-1)-po(i,j-2)) + ...
+                b*(-po(i+2,j)+16*po(i+1,j)-30*po(i,j)+16*po(i-1,j)-po(i-2,j)) + ...
+                c*(6*po(i,j)-23*pm(i,j)+34*pmm(i,j)-24*pmmm(i,j)+8*pmmmm(i,j)-pmmmmm(i,j)) + ...
+                d*(2*po(i,j)^2-5*pm(i,j)^2+4*pmm(i,j)^2-pmmm(i,j)^2); 
         end
     end   
     
     % update info
-    pm=po; po=pn;
+    pmmmmm=pmmmm; pmmmm=pmmm; pmmm=pmm; pmm=pm; pm=po; po=pn;
 
     % Update dt and iteration counter
-    if t+dt>tFinal; dt=tFinal-t; end; t=t+dt; it=it+1;
+    if t+dt>tFinal; dt=tFinal-t; % update coefs 
+    a=(c0*dt/dx)^2/12; b=(c0*dt/dr)^2/12; c=(delta/(8*dt*c0^2)); end; 
+    t=t+dt; it=it+1;
     
     % Perturbation in time
-    po(device) = sin(1E6*2*pi*t)*exp(-(t-2E-6)^2);
+    po(device) = sin(1E6*2*pi*t)*exp(-((t-4E-6)/1.5E-6)^2); 
     
     % plot figure
     if plotfigs~=false
